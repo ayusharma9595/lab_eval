8e9f01254d69797e3f14f1912dd13978f5021172
ayusharma9595 <114064998+ayusharma9595@users.noreply.github.com>
2022-11-17 22:54:15 +0530

    update the file

--- Hellowrld.java
+++ Hellowrld.java
@@ -1,7 +1,5 @@
 public class Hellowrld{
 public static void main(String [] args){
-System.out.println("It's a bug !");
-System.out.println("It is modified from remote to local");
-System.out.println("This is new java file");
+System.out.println("Optimism is an occupational hazard of programming: feedback is the treament.");
 }
 }
